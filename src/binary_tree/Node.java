f7d132c7173f868717ea6cb0d838a028ba3091a6
Vlad <147541054+kvs23031999@users.noreply.github.com>
2024-03-24 15:44:57 +0200

    initial commit

---
+++ src/binary_tree/Node.java
@@ -0,0 +1,14 @@
+package binary_tree;
+
+public class Node {
+
+    int item;
+    Node left;
+    Node right;
+
+    public Node(int key) {
+        this.item = key;
+        this.left = null;
+        this.right = null;
+    }
+}
